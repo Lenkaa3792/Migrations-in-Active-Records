70fa22a6385956deb074b10afb1b96d336bfc21d
Lenkaa3792 <allanlenkaa@gmail.com>
2022-08-30 11:55:39 +0300

    first commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+gem "activerecord"
+gem "sinatra"
+gem "rake"
+gem "sinatra-activerecord"
+gem "sqlite3"
---
+++ db/migrate/20220830084023_create_cohorts.rb
@@ -0,0 +1,8 @@
+class CreateCohorts < ActiveRecord::Migration[7.0]
+  def change
+     create_table :cohorts do |t|
+      t.string :name
+     
+  end
+  end
+end
